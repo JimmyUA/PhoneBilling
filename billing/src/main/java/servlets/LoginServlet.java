cde5ac52901b040593d8fa815a63ae48e78cf879
jimmy <sergii.pryhodko@gmail.com>
2017-08-21 15:42:14 +0300

    standard drafts

--- billing/src/main/java/servlets/LoginServlet.java
+++ billing/src/main/java/servlets/LoginServlet.java
@@ -1,4 +1,23 @@
 package servlets;
 
-public class LoginServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        
+    }
 }
