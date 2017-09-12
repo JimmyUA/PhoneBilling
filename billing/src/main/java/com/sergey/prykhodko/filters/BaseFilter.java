c6330ec98c217aaf32832930481623b972124064
jimmy <sergii.pryhodko@gmail.com>
2017-09-12 13:39:45 +0300

    Base filter to have implementation of init() and destroy()

--- billing/src/main/java/com/sergey/prykhodko/filters/BaseFilter.java
+++ billing/src/main/java/com/sergey/prykhodko/filters/BaseFilter.java
@@ -1,4 +1,22 @@
 package com.sergey.prykhodko.filters;
 
-public class BaseFilter {
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class BaseFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // skipped
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        //skipped
+    }
 }
