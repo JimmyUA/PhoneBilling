e865f4560c09a1ba26d502acee376c48baabe608
jimmy <sergii.pryhodko@gmail.com>
2017-09-18 19:06:17 +0300

    admin cabinet and TariffsController is connected

--- billing/src/main/java/com/sergey/prykhodko/controller/admin/TariffsController.java
+++ billing/src/main/java/com/sergey/prykhodko/controller/admin/TariffsController.java
@@ -1,4 +1,16 @@
 package com.sergey.prykhodko.controller.admin;
 
-public class TariffsController {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "tariffs", urlPatterns = "/tariffs")
+public class TariffsController extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Tariffs");
+    }
 }
