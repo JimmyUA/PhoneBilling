c0c012aa382453ae420c682bdcb10e518a0ab0be
jimmy <sergii.pryhodko@gmail.com>
2017-09-19 20:59:45 +0300

    classes for tariffs handling

---
+++ billing/src/main/java/com/sergey/prykhodko/controller/admin/CreateTariff.java
@@ -0,0 +1,4 @@
+package com.sergey.prykhodko.controller.admin;
+
+public class CreateTariff {
+}
--- billing/src/main/java/com/sergey/prykhodko/dao/factories/TariffPlanMySqlDAO.java
+++
@@ -1,6 +0,0 @@
-package com.sergey.prykhodko.dao.factories;
-
-import com.sergey.prykhodko.dao.TariffPlanDAO;
-
-public class TariffPlanMySqlDAO extends TariffPlanDAO {
-}
